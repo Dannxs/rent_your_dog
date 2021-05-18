2f8ac4d928d5ece9efe28da1d0a21a1c45a50fb5
Danny Cardoso <dcardoso066@gmail.com>
2021-05-18 14:47:58 +0200

    ajout information user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_085838) do
+ActiveRecord::Schema.define(version: 2021_05_18_113354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,8 @@ ActiveRecord::Schema.define(version: 2021_05_18_085838) do
     t.float "rating"
     t.text "comment"
     t.string "address"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
